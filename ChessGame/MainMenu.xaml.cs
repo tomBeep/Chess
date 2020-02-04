80b9d18026bb1a790561d072734764dd0c173830
Thomas Edwards <thomase@outlook.co.nz>
2020-02-04 16:57:55 +1300

    Add project files.

---
+++ ChessGame/MainMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessGame
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Game (object sender, RoutedEventArgs e)
+        {
+            // Start a new Game
+            GameBoard game = new GameBoard();
+            this.NavigationService.Navigate(game);
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
